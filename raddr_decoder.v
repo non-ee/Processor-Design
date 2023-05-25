e6ae2097ce5cd3e1026fd8bdcffa8e5f97e9469d
non.ee <nonnam.en9@gmail.com>
2023-05-26 02:48:43 +0900

    debugg all stages modules

--- raddr_decoder.v
+++
@@ -1,9 +0,0 @@
-module raddr_decoder(inst, rs1, rs2, rd);
-    input [31:0] inst;
-    output [4:0] rs1, rs2, rd;
-
-    assign rd  = inst[11:7];
-    assign rs1 = inst[19:15];
-    assign rs2 = inst[24:20];
-    
-endmodule
